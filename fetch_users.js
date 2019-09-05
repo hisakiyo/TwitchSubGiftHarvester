b669c84856b2a117610acd579b85f2b7cc7e07e8
Hisakiyo <gauthier.marette@protonmail.ch>
2019-09-05 09:57:45 +0100

    Add existing file

---
+++ fetch_users.js
@@ -0,0 +1,58 @@
+/* Fetch 100 streamers by views*/
+
+const https = require('https')
+var ircClient = require('node-irc');
+const fs = require('fs');
+
+const oauth = ''
+
+const options = {
+  hostname: 'api.twitch.tv',
+  port: 443,
+  path: '/kraken/streams/?limit=100',
+  method: 'GET',
+  headers: {
+    'Accept' : 'application/vnd.twitchtv.v5+json',
+    'Client-ID' : 'zsujit5vv4gtew8k6snn8xr0pb7q9a'
+  }
+};
+
+
+const req = https.request(options, (res) => {
+  let data = '';
+  res.on('data', (chunk) => {
+        data+=chunk;
+  });
+  res.on('end', () => {
+
+
+fs.writeFile('user.txt', '[', function (err) {
+  if (err) throw err;
+});
+
+        var dparse = JSON.parse(data)
+        for(var i=0;i<100;i++){
+                var user = dparse.streams[i].channel.name
+		if(i<99){
+		fs.appendFile("user.txt", '\"#' + user+ '\"' + ',', function(err) {
+    			if(err) {
+        			return console.log(err);
+	    		}
+		}); 
+		} else {
+		fs.appendFile("user.txt", '\"#' + user+ '\"' + ']', function(err) {
+                        if(err) {
+                                return console.log(err);
+                        }
+                });
+		console.log("Fichier sauvegardé");
+	        }
+	}
+  });
+});
+
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+req.end();
---
+++ irc_connect.js
@@ -0,0 +1,55 @@
+var irc = require("irc");
+const fs = require("fs");
+var users = fs.readFileSync("user.txt").toString();
+users = JSON.parse(users)
+console.log(users)
+
+
+var config = {
+	channels: users,
+	server: "irc.chat.twitch.tv",
+	oauth: "oauth:",
+	botName: "hisakiyo_",
+	autoRejoin: false,
+	autoConnect: true,
+	floodProtection: true,
+	floodProtectionDelay: 3000
+};
+
+console.log(config.channels[0])
+
+var botClient = new irc.Client(config.server, config.botName, {
+	channels: config.channels,
+	password: config.oauth
+});
+
+function join(channel, nick, msgobj)
+{
+	if (nick.toLowerCase() == config.botName.toLowerCase())
+	{
+		console.log("Bot has joined channel", channel);
+	}
+}
+
+
+function msg(from, channel, text, message)
+{
+/*	if (text == "test") {
+		console.log(channel + " - input: test - output: Test successful");
+		botClient.send("PRIVMSG" , channel, "Test successful");
+	}
+*/
+}
+
+
+function err(message)
+{
+	console.log("Error: ", message);
+}
+
+botClient.addListener("message", msg);
+botClient.addListener("action", msg);
+botClient.addListener("join", join);
+botClient.addListener("error", err);
+
+
