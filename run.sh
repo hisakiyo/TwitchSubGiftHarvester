44235b6ddcb92bf1fa0203e86f52bb8a22e44b89
hisakiyo <34187647+hisakiyo@users.noreply.github.com>
2019-09-06 09:28:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo pkill -f TwitchBot/irc_connect.js || echo "Process was not running."
-node /home/pi/TwitchBot/fetch_users.js
-node /home/pi/TwitchBot/irc_connect.js &
+node fetch_users.js
+node irc_connect.js &
